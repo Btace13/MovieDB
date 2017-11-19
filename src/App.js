799ff461534cc64acc95809b6e68034052b091a5
Enrique Urbina <btace1347@yahoo.com>
2017-11-19 12:41:07 -0600

    grabbing the response data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import Header from './components/Header'
 import Card from './components/Card'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-            text: 'Ace'
-    };
-}
-myCallback = (newText) => {
-  this.setState({text: newText});
-};
   render() {
     return (
       <div className="App">
           <Navbar/>
           <div className="container">
-          <h1 handleNewText={this.myCallback}>{this.state.text}</h1>
-              <Header/>
               <Card/>
           </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,47 @@
 import React, {Component} from 'react';
+import  axios from 'axios';
 
-class Card extends Component{ 
+class Card extends Component{
     constructor(props){
-    super(props);
-    this.state = {
-        newText: ''
+        super(props);
+        this.state = {
+            title: 'Ace'
+        }
     }
-}
-handleTheNewText = (newerText) => {
-    this.setState({
-        newText: newerText
-    });
-};
-render(){
-
+    componentWillMount(){
+        axios.get('https://api.themoviedb.org/3/movie/550?api_key=ea009f1bd0f8b5df0bc7838d68828108')
+            .then((response) => {
+                 this.setState({title: response.data.title});
+                console.log(response);
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+    render(){
         return(
-
-           <main>
-             <div className="row">
-                 <h1 className={'center'}>Howard The Duck</h1>
-                 <div className="col m4 s12">
-                     <img className={'responsive-img'} src="https://thedrunkenodyssey.files.wordpress.com/2015/05/howard1.jpg" alt="Howey"/>
-                 </div>
-                 <div className="col m8 s12">
-                    <h4>You will believe that a duck can talk.</h4>
-                     <p className="flow-text">A scientific experiment unknowingly brings extraterrestrial life forms to the Earth through a laser beam. First is the cigar smoking drake Howard from the duck's planet. A few kids try to keep him from the greedy scientists and help him back to his planet. But then a much less friendly being arrives through the beam...</p>
+        <div>
+            <header>
+                <div className="row">
+                    <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="email" type="text" className="validate" placeholder={'Movie Name'}/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </header>
+            <main>
+                <h1>{this.state.title}</h1>
+                <div className="row">
+                    <h1 className={'center'}>Howard The Duck</h1>
+                    <div className="col m4 s12">
+                        <img className={'responsive-img'} src="https://thedrunkenodyssey.files.wordpress.com/2015/05/howard1.jpg" alt="Howey"/>
+                    </div>
+                    <div className="col m8 s12">
+                        <h4>You will believe that a duck can talk.</h4>
+                        <p className="flow-text">A scientific experiment unknowingly brings extraterrestrial life forms to the Earth through a laser beam. First is the cigar smoking drake Howard from the duck's planet. A few kids try to keep him from the greedy scientists and help him back to his planet. But then a much less friendly being arrives through the beam...</p>
                         <div className="row">
                             <div className="col s6">
                                 <h5>Original Release:</h5>
@@ -43,9 +60,10 @@ render(){
                                 <p>5.1 / 10</p>
                             </div>
                         </div>
-                 </div>
-             </div>
-           </main>
+                    </div>
+                </div>
+            </main>
+        </div>
 
         )
     }
